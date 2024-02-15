e9611c188421c78683a0beb489b856e55e10adeb
dohyun96305 <dohyun96305@naver.com>
2024-02-15 09:09:53 +0000

    Fix : args, gitignore

--- args.py
+++ args.py
@@ -17,16 +17,16 @@ def parse_args() :
     parser.add_argument('--heldout_users', default= 0, type=int)
 
     # run
-    parser.add_argument('--hidden-dim', type=int, default=600)
-    parser.add_argument('--latent-dim', type=int, default=300)
-    parser.add_argument('--batch-size', type=int, default=500)
+    parser.add_argument('--hidden_dim', type=int, default=600)
+    parser.add_argument('--latent_dim', type=int, default=300)
+    parser.add_argument('--batch_size', type=int, default=500)
     parser.add_argument('--beta', type=float, default=None)
     parser.add_argument('--gamma', type=float, default=0.005)
     parser.add_argument('--lr', type=float, default=5e-4)
     parser.add_argument('--n_epochs', type=int, default=5)
     parser.add_argument('--n_enc_epochs', type=int, default=3)
     parser.add_argument('--n_dec_epochs', type=int, default=1)
-    parser.add_argument('--not-alternating', type=bool, default=False)
+    parser.add_argument('--not_alternating', type=bool, default=False)
     parser.add_argument('--optimizer', type=str, default='Adam', help='optimizer type (default: Adam)') # optimizer 설정
     
     args = parser.parse_args()
